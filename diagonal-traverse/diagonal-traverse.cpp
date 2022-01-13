7aada1a1069dccbedbe9b267eb59c2a0fa2c5f4d
Anjali Jain <jainanjali13224@gmail.com>
2022-01-14 02:29:16 +0530

    Time: 63 ms, Memory: 22.4 MB - LeetHub

---
+++ diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        
+        int m = mat.size(), n = mat[0].size();
+        
+        vector<vector<int>> vec(m+n, vector<int>());
+        vector<int> ans;
+        
+        for(int i=0; i<m; i++){
+            
+            for(int j =0; j<n; j++){
+                
+                vec[i+j].push_back(mat[i][j]);
+            }
+        }
+        
+        for(int i =0; i<vec.size(); i++){
+            
+            if(i%2 ==0)
+                reverse(vec[i].begin(), vec[i].end());
+            for(int j =0; j< vec[i].size(); j++)
+                ans.push_back(vec[i][j]);
+        }
+        
+        return ans;
+    }
+};
